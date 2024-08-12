8734f2f2752949205880a2fbca758b4f66a72e5f
hnayoung <hnaa0@naver.com>
2024-08-12 17:51:17 +0900

    🎨 chore: configure ThemeData, fonts, and theme colors

---
+++ lib/common/themes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+abstract class ComentitoDiaryTheme {
+  static ThemeData light = ThemeData(
+    brightness: Brightness.light,
+    fontFamily: "Pretendard",
+    useMaterial3: true,
+  );
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily: "Pretendard",
+    useMaterial3: true,
+  );
+}
---
+++ lib/constants/theme_colors.dart
@@ -0,0 +1,17 @@
+class ThemeColors {
+  static const white = 0xffffffff;
+  static const grey_100 = 0xfff5f5f5;
+  static const grey_200 = 0xffe8ebeb;
+  static const grey_300 = 0xffd7dada;
+  static const grey_400 = 0xffbfc1c1;
+  static const grey_500 = 0xffa6a8a8;
+  static const grey_600 = 0xff8c8e8e;
+  static const grey_700 = 0xff6f7070;
+  static const grey_800 = 0xff545555;
+  static const grey_900 = 0xff444545;
+  static const black = 0xff000000;
+  static const green = 0xff6ebb92;
+  static const mintBlue = 0xffbce5d7;
+  static const mintGreen = 0xffd6f2ed;
+  static const mintCream = 0xfff6faf8;
+}
